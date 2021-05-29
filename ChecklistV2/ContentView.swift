150f939083a71b2e6a9b77143b6410c6fe79be15
Dong-Hyeon Kim <feira@naver.com>
2021-05-29 22:14:01 +0900

    Initial Commit

---
+++ ChecklistV2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChecklistV2
+//
+//  Created by RelMac User Exercise3 on 2021/05/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
